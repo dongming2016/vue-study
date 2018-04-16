f242838110d0a55129e3560917a9c4b26e0f4836
chenmin <350376345@qq.com>
2018-04-16 13:01:50 +0800

    新增弹框和注册页面

---
+++ src/components/alertbox/alertbox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <div class="mask"></div>
+      <div class="alertbox">
+          <div class="alertbox-content">
+            <div class="alertbox-title" v-if="alertbox.title.show">{{alertbox.title.content}}</div>
+            <slot name="content"></slot>
+            <div class="alertbox-footer"></div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['alertbox']
+}
+</script>
+
+<style lang="postcss" scoped>
+    .alertbox-title {
+        border-left: palegreen 8px solid;
+        background: seashell;
+        height: 24px;
+    }
+    .alertbox-content {
+        margin-left: -50%;
+        min-width: 140px;
+        background: #fff;
+        box-shadow: 4px 4px 8px 0 grey
+    }
+    .mask {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 9998;
+        left: 0;
+        top: 0;
+        background-attachment: scroll;
+        background: #767676;
+        opacity: 0.4;
+    }
+    .alertbox {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        z-index: 9999;
+    }
+</style>
--- src/pages/register/register.vue
+++ src/pages/register/register.vue
@@ -1,5 +1,70 @@
 <template>
-  <div>
-      <timeline></timeline>
+  <div class="register-container">
+    <div class="register-box">
+      <div class="register-inputbox">
+        <label class="label"><span class="register-star">* </span>用户名</label>
+        <input type="text" required placeholder="请输入用户名" v-model="user.name">
+      </div>
+      <div class="register-inputbox">
+        <label class="label"><span class="register-star">* </span>密码</label>
+        <input type="password" required placeholder="请输入密码" v-model="user.password">
+      </div>
+      <div class="register-inputbox">
+        <label class="label"><span class="register-star">* </span>重复密码</label>
+        <input type="password" required placeholder="请再次输入密码" v-model="user.repassword">
+      </div>
+    </div>
+    <div><div class="button">确定</div></div>
+    <div>
+      <router-link to='userinfo'>其它信息</router-link>
+    </div>
+    <alertbox v-bind:alertbox="alertbox" v-if="alertbox.show">
+        <div slot="content">hello</div>
+    </alertbox>
   </div>
 </template>
+
+<script>
+import alertbox from '../../components/alertbox/alertbox'
+export default {
+    data: function () {
+        return { user: {name: '', password: '', repassword: ''}, errorInfo: {password: ''}, alertbox: {show: false, title: {show: true}} }
+    },
+    methods: {
+        OK: function () {
+            if (this.user.password !== this.user.repassword) {
+                this.errorInfo.password = '密码不一致'
+            }
+        }
+    },
+    components: {
+        alertbox
+    }
+}
+</script>
+
+<style lang="postcss" scoped>
+  .label {
+    width: 80px;
+    display: inline-block;
+    text-align: right;
+    margin-right: 4px;
+  }
+  .register-container {
+      margin-top: 50vh;
+      width: 300px;
+      margin-left: auto;
+      margin-right: auto;
+  }
+  .register-box {
+      margin-top: -20%;
+  }
+  .register-star {
+    color: red;
+    vertical-align: middle;
+    vertical-align: -webkit-baseline-middle;
+  }
+  .register-inputbox {
+      height: 33px;
+  }
+</style>
